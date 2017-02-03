c21390de2490ab66eb332fb833b0201915e93a46
Ben Fitzgerald <theeuclidean@gmail.com>
2017-02-03 22:32:05 +0100

    Adding react-bootstrap

--- js/components/App.js
+++ js/components/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import Relay from 'react-relay';
+import { Button,
+         Grid,
+         Col,
+         Row,
+         PageHeader,
+         ListGroupItem,
+         ListGroup }
+         from 'react-bootstrap';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Widget list</h1>
-        <ul>
-          {this.props.viewer.widgets.edges.map(edge =>
-            <li key={edge.node.id}>{edge.node.name} (ID: {edge.node.id})</li>
-          )}
-        </ul>
-      </div>
+      <Col xs={8} xsOffset={2}>
+        <Row className="show-grid">
+        <PageHeader>Widget list</PageHeader>
+          <ListGroup>
+            {this.props.viewer.widgets.edges.map(edge =>
+              <ListGroupItem key={edge.node.id}>{edge.node.name} (ID: {edge.node.id})</ListGroupItem>
+            )}
+          </ListGroup>
+        </Row>
+      </Col>
     );
   }
 }
